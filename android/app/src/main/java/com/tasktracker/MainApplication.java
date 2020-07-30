ad182327662347bfb3e9a2c61e40409262f5093f
GeraJuarez <geracjuarez@gmail.com>
2020-07-29 22:37:19 -0500

    trying paypal

--- android/app/src/main/java/com/tasktracker/MainApplication.java
+++ android/app/src/main/java/com/tasktracker/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.smarkets.paypal.RNPaypalPackage;
+import com.smarkets.paypal.RNPaypalPackage;
 import com.sbugert.rnadmob.RNAdMobPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
